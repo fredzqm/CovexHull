009313cd2cf267f500cad9db6fb7f0e372da6fee
Fred Zhang <fredzqm@gmail.com>
2017-02-27 15:41:01 -0500

    create convexHull instance

--- src/ConvexHull.java
+++ src/ConvexHull.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,7 +12,110 @@ import java.util.Scanner;
  * 
  * @author FredZhang
  */
-public class ConvexHull {
+public class ConvexHull implements Iterable<Point> {
+	private List<Point> convex;
+
+	public ConvexHull(List<Point> points) {
+		this.convex = run(points);
+	}
+
+	public int size() {
+		return convex.size();
+	}
+
+	@Override
+	public Iterator<Point> iterator() {
+		return convex.iterator();
+	}
+
+	/**
+	 * Use quick hull method to solve this problem. Divide-and-conquer. Divide
+	 * set into two parts by an exteme line and run the same algorithm on two
+	 * sets separtely.
+	 * 
+	 * @param points
+	 * @return
+	 */
+	private static List<Point> run(Collection<Point> points) {
+		// get the left-most and right-most points in the set
+		int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+		Point minP = null, maxP = null;
+		for (Point p : points) {
+			if (p.x < min) {
+				min = p.x;
+				minP = p;
+			} else if (p.x == min) {
+				if (p.y < minP.y)
+					minP = p;
+			}
+			if (p.x > max) {
+				max = p.x;
+				maxP = p;
+			} else if (p.x == max) {
+				if (p.y < maxP.y)
+					maxP = p;
+			}
+		}
+
+		// recursively call helper method, and concatanate all vertice
+		// together in order.
+		List<Point> conv = new ArrayList<Point>();
+		conv.add(minP);
+		conv.addAll(VerticeOnRight(points, minP, maxP));
+		conv.add(maxP);
+		conv.addAll(VerticeOnRight(points, maxP, minP));
+		return conv;
+	}
+
+	/**
+	 * 
+	 * @param points
+	 * @param p1
+	 * @param p2
+	 * @return all vertece on the right side of P1P2
+	 */
+	private static List<Point> VerticeOnRight(Collection<Point> points, Point p1, Point p2) {
+		List<Point> m = new ArrayList<Point>();
+		int a = p2.y - p1.y;
+		int b = p1.x - p2.x;
+		int c = p1.x * p2.y - p1.y * p2.x;
+		int max = 0;
+		Point maxP = null;
+		for (Point p : points) {
+			int s = a * p.x + b * p.y - c;
+			if (s > 0) {
+				m.add(p);
+				if (s > max) {
+					max = s;
+					maxP = p;
+				} else if (s == max) {
+					if (distsqr(p1, p) < distsqr(p1, maxP)) {
+						max = s;
+						maxP = p;
+					}
+				}
+			}
+		}
+		List<Point> conv = new ArrayList<Point>();
+		// basis case, no more points on the right side
+		if (maxP == null)
+			return conv;
+		conv.addAll(VerticeOnRight(m, p1, maxP));
+		conv.add(maxP);
+		conv.addAll(VerticeOnRight(m, maxP, p2));
+		return conv;
+	}
+
+	/**
+	 * 
+	 * @param a
+	 * @param b
+	 * @return the square of distance between a and b
+	 */
+	private static int distsqr(Point a, Point b) {
+		return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
+	}
+
 	@SuppressWarnings("resource")
 	public static void main(String[] args) throws FileNotFoundException {
 		// import the list of points
@@ -20,17 +124,17 @@ public class ConvexHull {
 		String name = in.nextLine();
 		List<Point> points = readPoints("input/" + name + ".txt");
 		System.out.println("successfully import " + points.size() + " points from " + name + ".txt");
-
+	
 		// solve the problem and record the time elapsed.
 		long time = System.nanoTime();
 		System.out.print("Quick hull method");
-		List<Point> convex = QuickHull.run(points);
+		ConvexHull convex = new ConvexHull(points);
 		long t = System.nanoTime() - time;
 		System.out.println(" produces " + convex.size() + " vertice convex hull in " + t / 1000000.0 + " milliseconds");
-
+	
 		// display the result in frame.
 		ConvexHullFrame display = new ConvexHullFrame(points);
-		display.drawConvex(convex);
+		display.drawConvex(convex, convex.size());
 	}
 
 	@SuppressWarnings("resource")
@@ -52,96 +156,6 @@ public class ConvexHull {
 			}
 		}
 		return points;
-	}
-
-	static class QuickHull {
-		/**
-		 * Use quick hull method to solve this problem. Divide-and-conquer.
-		 * Divide set into two parts by an exteme line and run the same
-		 * algorithm on two sets separtely.
-		 * 
-		 * @param points
-		 * @return
-		 */
-		private static List<Point> run(Collection<Point> points) {
-			// get the left-most and right-most points in the set
-			int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
-			Point minP = null, maxP = null;
-			for (Point p : points) {
-				if (p.x < min) {
-					min = p.x;
-					minP = p;
-				} else if (p.x == min) {
-					if (p.y < minP.y)
-						minP = p;
-				}
-				if (p.x > max) {
-					max = p.x;
-					maxP = p;
-				} else if (p.x == max) {
-					if (p.y < maxP.y)
-						maxP = p;
-				}
-			}
-
-			// recursively call helper method, and concatanate all vertice
-			// together in order.
-			List<Point> conv = new ArrayList<Point>();
-			conv.add(minP);
-			conv.addAll(VerticeOnRight(points, minP, maxP));
-			conv.add(maxP);
-			conv.addAll(VerticeOnRight(points, maxP, minP));
-			return conv;
-		}
-
-		/**
-		 * 
-		 * @param points
-		 * @param p1
-		 * @param p2
-		 * @return all vertece on the right side of P1P2
-		 */
-		private static List<Point> VerticeOnRight(Collection<Point> points, Point p1, Point p2) {
-			List<Point> m = new ArrayList<Point>();
-			int a = p2.y - p1.y;
-			int b = p1.x - p2.x;
-			int c = p1.x * p2.y - p1.y * p2.x;
-			int max = 0;
-			Point maxP = null;
-			for (Point p : points) {
-				int s = a * p.x + b * p.y - c;
-				if (s > 0) {
-					m.add(p);
-					if (s > max) {
-						max = s;
-						maxP = p;
-					} else if (s == max) {
-						if (distsqr(p1, p) < distsqr(p1, maxP)) {
-							max = s;
-							maxP = p;
-						}
-					}
-				}
-			}
-			List<Point> conv = new ArrayList<Point>();
-			// basis case, no more points on the right side
-			if (maxP == null)
-				return conv;
-			conv.addAll(VerticeOnRight(m, p1, maxP));
-			conv.add(maxP);
-			conv.addAll(VerticeOnRight(m, maxP, p2));
-			return conv;
-		}
-
-		/**
-		 * 
-		 * @param a
-		 * @param b
-		 * @return the square of distance between a and b
-		 */
-		private static int distsqr(Point a, Point b) {
-			return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
-		}
 	}
 
 }
--- src/ConvexHullFrame.java
+++ src/ConvexHullFrame.java
@@ -9,6 +9,7 @@ import javax.swing.JFrame;
 
 /**
  * GUI frame to display convex problem
+ * 
  * @author FredZhang
  *
  */
@@ -21,30 +22,29 @@ public class ConvexHullFrame extends JFrame {
 	static final int Height = 800;
 	double point_d;
 	double ratio;
-	private List<Point> points;
+	private Iterable<Point> points;
 	private int[] convexX;
 	private int[] convexY;
 	private Canvas canvas;
 
 	/**
 	 * construct a convex hull frame and display it.
+	 * 
 	 * @param points
 	 */
-	public ConvexHullFrame(List<Point> points) {
+	public ConvexHullFrame(Iterable<Point> points) {
 		this.points = points;
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		int maxX = points.get(0).x;
-		int maxY = points.get(0).y;
+		int maxX = Integer.MIN_VALUE, maxY = Integer.MIN_VALUE;
 		// find an appropriate frame size
-		for (int i = 1; i < points.size(); i++) {
-			Point p = points.get(i);
+		for (Point p : points) {
 			if (p.y > maxY)
 				maxY = p.y;
 			if (p.x > maxX)
 				maxX = p.x;
 		}
 		// set a shrinking ratio, to adjust the graph to an appropriate size
-		ratio = Math.min(Width/(maxX+50.0), Height/(maxY+50.0));
+		ratio = Math.min(Width / (maxX + 50.0), Height / (maxY + 50.0));
 		point_d = 8 * ratio;
 		setSize(Width, Height);
 		canvas = new Canvas();
@@ -55,21 +55,24 @@ public class ConvexHullFrame extends JFrame {
 
 	/**
 	 * specify verteces in the convex hull, and display it.
+	 * 
 	 * @param convex
 	 */
-	public void drawConvex(List<Point> convex) {
-		convexX = new int[convex.size()];
-		convexY = new int[convex.size()];
-		for (int i = 0; i < convex.size(); i++) {
-			Point p = convex.get(i);
+	public void drawConvex(Iterable<Point> convex, int size) {
+		convexX = new int[size];
+		convexY = new int[size];
+		int i = 0;
+		for (Point p : convex) {
 			convexX[i] = (int) (p.x * ratio);
 			convexY[i] = (int) (p.y * ratio);
+			i++;
 		}
 		repaint();
 	}
-	
+
 	/**
 	 * the JCompoennt that does all the painting
+	 * 
 	 * @author FredZhang
 	 */
 	private class Canvas extends JComponent {
@@ -84,17 +87,17 @@ public class ConvexHullFrame extends JFrame {
 			Graphics2D g2 = (Graphics2D) g;
 			for (Point i : points) {
 				// all points
-				g2.fill(new Arc2D.Double((i.x *ratio - point_d / 2), i.y * ratio - point_d / 2, point_d,
-						point_d, 0, 360, 0));
+				g2.fill(new Arc2D.Double((i.x * ratio - point_d / 2), i.y * ratio - point_d / 2, point_d, point_d, 0,
+						360, 0));
 			}
 			g2.setColor(Color.RED);
 			if (convexX != null) {
 				// the convex hull
 				g2.drawPolygon(convexX, convexY, convexX.length);
-				for(int i = 0; i < convexX.length ; i ++){
+				for (int i = 0; i < convexX.length; i++) {
 					// the vertice on the convex hull
-					g2.fill(new Arc2D.Double( convexX[i] - point_d / 2, convexY[i] - point_d / 2, point_d,
-							point_d, 0, 360,0));
+					g2.fill(new Arc2D.Double(convexX[i] - point_d / 2, convexY[i] - point_d / 2, point_d, point_d, 0,
+							360, 0));
 				}
 			}
 		}
